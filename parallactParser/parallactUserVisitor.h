772bc6ed6863eb25ee3b21928d92a157bbf7abfa
Evgeniy Volodin <zheka.az@mail.ru>
2024-03-20 20:44:32 +0300

    Add MPI code

--- parallactParser/parallactUserVisitor.h
+++ parallactParser/parallactUserVisitor.h
@@ -10,6 +10,8 @@
 class parallactUserVisitor : public parallactBaseVisitor {
 public:
     std::string code;
+    bool MPI = false;
+    bool OpenMP = false;
 
     virtual std::any visitParallelMpiOmp(parallactParser::ParallelMpiOmpContext *ctx) override {
         if (ctx->standart->getType() == 2) {
@@ -17,7 +19,25 @@ public:
             code += "int rank, size;\n"
                     "MPI_Init();\n"
                     "MPI_Comm_rank(MPI_COMM_WORLD, &rank);\n"
-                    "MPI_Comm_size(MPI_COMM_WORLD, &size);";
+                    "MPI_Comm_size(MPI_COMM_WORLD, &size);\n";
+            MPI = true;
+        }
+        return visitChildren(ctx);
+    }
+
+    virtual std::any visitForStatement(parallactParser::ForStatementContext* ctx) override {
+        std::cout << "We on for" << std::endl;
+        if (MPI) {
+            code += "int n = " + ctx->expression()->expression().at(1)->getText() + ";\n";
+            code += "int local_n = n / size;\n";
+            code += "int start = rank * local_n;\n";
+            code += "int end = (rank + 1) * local_n;\n";
+            code += "for (" + ctx->type()->getText() + " "
+                + ctx->assignment().at(0)->IDENTIFIER()->getText() + " = " + "start; "
+                + ctx->expression()->expression().at(0)->getText() + " < " + "end; "
+                + ctx->assignment().at(1)->IDENTIFIER()->getText() + "++)" + " {\n";
+            code += "\t // your code\n";
+            code += "}";
         }
         return 0;
     }
